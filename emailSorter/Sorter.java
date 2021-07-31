c3957db6cd0d2fbce18140d27d98d14f670671e2
Ethan Wang <ethan_wang@berkeley.edu>
2021-07-30 17:23:36 -0700

    setup

---
+++ emailSorter/Sorter.java
@@ -0,0 +1,15 @@
+package emailSorter;
+
+
+/**
+ * This file takes in the input.txt file and sorts all valid email by domain, outputting each email 
+ * in order line by line through output.txt
+ * @author Ethan Wang
+ */
+
+
+public class Sorter {
+
+
+
+}
